2a430a75015c947a0a510b24fe4ace4f4e056ce0
Saurav das <dassaurav4534@gmail.com>
2026-05-25 02:13:07 +0530

    Change NextAuthConfig to NextAuthOptions type

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import { type NextAuthConfig } from "next-auth";
+import { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authConfig: NextAuthConfig = {
+export const authConfig: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
