e5f0a48f5df7be5fc4c31bcdfbb56ec9f2082646
Saurav das <dassaurav4534@gmail.com>
2026-05-25 02:35:24 +0530

    Add TypeScript definitions for NextAuth user and session

---
+++ types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { Role } from "./index";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: Role;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    role: Role;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: Role;
+  }
+}
